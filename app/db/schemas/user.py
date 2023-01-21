53d17761d5f827b91bb8d57030aa079852605a54
Manas <manasshyam25@gmail.com>
2023-01-21 01:08:04 -0500

    added email to user table

--- app/db/schemas/user.py
+++ app/db/schemas/user.py
@@ -11,6 +11,7 @@ class User(db.Model):
     id_: str = db.Column(TEXT, unique=True, nullable=False, primary_key=True)
     user: str = db.Column(db.String(50), unique=True, nullable=False)
     name: str = db.Column(db.String(100), nullable=False)
+    email: str = db.Column(db.TEXT, unique=True, nullable=False)
     password_hash: str = db.Column(TEXT, nullable=False)
     created_at: int = db.Column(db.Integer)
     is_admin: bool = db.Column(db.Boolean, default=False)
@@ -21,10 +22,11 @@ class User(db.Model):
         user: str = None,
         name: str = None,
         password_hash: str = None,
-    ):
+        email: str = None):
         self.id_ = token_urlsafe(20)
         self.user = user.lower()
         self.name = name
+        email = email
         self.password_hash = password_hash
         self.created_at = time()
 
@@ -34,6 +36,7 @@ class User(db.Model):
             "id_": self.id_,
             "name": self.name,
             "user": self.user,
+            "email": self.email,
             "created_at": self.created_at,
             "is_admin": self.is_admin,
             "_secure_": {},
